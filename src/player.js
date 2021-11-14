cd4a99c29dd3e698de99f78fe6476e14b9cb1dd8
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-14 16:58:09 +0000

    feat: Make Player class v0.1.0

---
+++ src/player.js
@@ -0,0 +1,9 @@
+// =============== Player Class v0.1.0 =============== //
+
+const Player = function(playerNumber) {
+    this.playerNumber = playerNumber;
+    this.points = 0;
+    this.guess;
+}
+
+module.exports = Player;
