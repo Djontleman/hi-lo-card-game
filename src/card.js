14480b76a2107215c6e337ff9474c573ed275f74
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-14 16:35:18 +0000

    feat: Complete Card class v0.1.0

---
+++ src/card.js
@@ -0,0 +1,9 @@
+// =============== Card Class =============== //
+
+const Card = function(value, suit, name) {
+    this.value = value;
+    this.suit = suit;
+    this.name = name;
+}
+
+module.exports = Card;
