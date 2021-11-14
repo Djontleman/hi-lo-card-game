2c2a62588a6adb431088f0bc10e2419b4004857e
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-14 23:16:24 +0000

    feat: Add functionality to choose number of players

--- src/game.js
+++ src/game.js
@@ -6,7 +6,9 @@ const StandardDeck = require("./standardDeck");
 const prompt = require("prompt-sync")();
 
 const Game = function() {
+    this.players = [];
     this.player;
+    this.playerNumber = 0; 
     this.continue = true;
     this.deck;
 
@@ -16,7 +18,11 @@ const Game = function() {
     console.log();
 
     // Number of players
-    let numberOfPlayers = prompt("How many players are there? (Currently only 1 player is generated) ");
+    let numberOfPlayers = parseInt(prompt("How many players are there? "));
+    for (i = 1; i <= numberOfPlayers; i++) {
+        this.players.push(new Player(i));
+    }
+    console.log(this.players);
     console.log();
 
     // House rules
@@ -37,9 +43,6 @@ const Game = function() {
     }
     console.log();
 
-    // Assign players
-    this.player = new Player(1);
-
     // Shuffle deck
     this.deck = shuffleDeck(this.deck);
 
@@ -49,6 +52,15 @@ const Game = function() {
 
     // Turns
     while(this.continue) {
+        
+        // Assign player to turn
+        if (this.playerNumber > 0 && this.playerNumber < this.players.length) {
+            this.playerNumber++;
+        } else {
+            this.playerNumber = 1;
+        }
+        this.player = this.players[this.playerNumber - 1];
+
         console.log("Player " + this.player.playerNumber + "'s turn starting...")
         this.player, this.deck = turn(this.player, this.deck);
         
@@ -71,7 +83,9 @@ const Game = function() {
 
     // Final points displayed
     console.log("Final points:");
-    console.log("Player " + this.player.playerNumber + ": " + this.player.points);
+    for (player of this.players) {
+        console.log("Player " + player.playerNumber + ": " + player.points);
+    }
     console.log();
 
     // Goodbye prompt
